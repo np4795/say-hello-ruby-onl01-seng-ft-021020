fbe24f20b813c2b5bf15e5983e72035597fa3a20
Nicholas Patel <np4795@yahoo.com>
2020-02-10 23:37:03 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -3,8 +3,4 @@ def say_hello(name)
 end
 say_hello("Kent Beck")
 
-def say_hello_language(name,language="Ruby programmer")
-  puts"Hello #{language}!"
-end
-say_hello_language(name,language="Ruby programmer")
   
